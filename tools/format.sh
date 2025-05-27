2927d7d86506c229924fa7fc9e93e3f49b59a319
kingdo.ubuntu <1440852110@qq.com>
2025-05-28 00:42:11 +0800

    优化格式化脚本，添加未追踪的文件

--- tools/format.sh
+++ tools/format.sh
@@ -4,5 +4,5 @@ SCRIPT_PATH=$(readlink -f "${BASH_SOURCE[0]}")
 PROJECT_ROOT=$(dirname $(dirname "$SCRIPT_PATH"))
 
 pushd "$PROJECT_ROOT" > /dev/null
-git ls-files -- '*.cpp' '*.h' '*.hpp' '*.c' | xargs clang-format -i
+{ git ls-files -- '*.cpp' '*.h' '*.hpp' '*.c' && git ls-files --others --exclude-standard -- '*.cpp' '*.h' '*.hpp' '*.c'; } | sort -u | xargs clang-format -i
 popd > /dev/null
